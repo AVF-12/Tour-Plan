ef92217419566a612cd92344736a95e44a52eb38
AVF-12 <alvictorovich@gmail.com>
2021-09-20 16:07:55 +0300

    create: keyboard slider control

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 
   // Navigation arrows
   navigation: {
